4e5960275e66b0eb7bf900637deaa78853b1a210
hyshiling <hyshiling@outlook.com>
2026-09-07 09:47:10 +0800

    A-test

--- entry/src/main/cpp/common/ts_fn.cpp
+++ entry/src/main/cpp/common/ts_fn.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "ts_fn.h"
 
 #include <string>
@@ -45,58 +44,3 @@ void TsFn::Release() {
         tsfn_ = nullptr;
     }
 }
-
-void TsFn::Abort() {
-    if (tsfn_ != nullptr) {
-        napi_release_threadsafe_function(tsfn_, napi_tsfn_abort);
-        tsfn_ = nullptr;
-    }
-}
-=======
-#include "ts_fn.h"
-
-#include <string>
-
-TsFn::TsFn(napi_env env, napi_value func, const char * name, JsCall js_call) {
-    napi_value resource_name = nullptr;
-    napi_create_string_utf8(env, name, NAPI_AUTO_LENGTH, &resource_name);
-
-    napi_status status = napi_create_threadsafe_function(
-        env,
-        func,
-        nullptr,
-        resource_name,
-        0,    // 无界队列
-        1,    // 初始线程数
-        nullptr,
-        nullptr,
-        nullptr,
-        js_call,
-        &tsfn_);
-
-    if (status != napi_ok) {
-        tsfn_ = nullptr;
-    }
-}
-
-TsFn::~TsFn() {
-    if (tsfn_ != nullptr) {
-        napi_release_threadsafe_function(tsfn_, napi_tsfn_release);
-        tsfn_ = nullptr;
-    }
-}
-
-void TsFn::Call(void * data) {
-    if (tsfn_ == nullptr) {
-        return;
-    }
-    napi_call_threadsafe_function(tsfn_, data, napi_tsfn_nonblocking);
-}
-
-void TsFn::Release() {
-    if (tsfn_ != nullptr) {
-        napi_release_threadsafe_function(tsfn_, napi_tsfn_release);
-        tsfn_ = nullptr;
-    }
-}
->>>>>>> cpp
--- entry/src/main/cpp/napi_init.cpp
+++ entry/src/main/cpp/napi_init.cpp
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-#include "napi/native_api.h"
-
-// 各模块的导出注册
-void RegisterModelApi(napi_env env, napi_value exports);
-void RegisterGenerateApi(napi_env env, napi_value exports);
-void RegisterServeApi(napi_env env, napi_value exports);
-
-// 模板示例：两数相加（保留以兼容模板页面，待 UI 重写后移除）
-static napi_value Add(napi_env env, napi_callback_info info)
-{
-    size_t argc = 2;
-    napi_value args[2] = {nullptr};
-    napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
-
-    double value0 = 0;
-    napi_get_value_double(env, args[0], &value0);
-
-    double value1 = 0;
-    napi_get_value_double(env, args[1], &value1);
-
-    napi_value sum = nullptr;
-    napi_create_double(env, value0 + value1, &sum);
-    return sum;
-}
-
-EXTERN_C_START
-static napi_value Init(napi_env env, napi_value exports)
-{
-    napi_property_descriptor desc[] = {
-        { "add", nullptr, Add, nullptr, nullptr, nullptr, napi_default, nullptr },
-    };
-    napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
-
-    RegisterModelApi(env, exports);
-    RegisterGenerateApi(env, exports);
-    RegisterServeApi(env, exports);
-    return exports;
-}
-EXTERN_C_END
-
-static napi_module demoModule = {
-    .nm_version = 1,
-    .nm_flags = 0,
-    .nm_filename = nullptr,
-    .nm_register_func = Init,
-    .nm_modname = "entry",
-    .nm_priv = ((void*)0),
-    .reserved = { 0 },
-};
-
-extern "C" __attribute__((constructor)) void RegisterEntryModule(void)
-{
-    napi_module_register(&demoModule);
-}
-=======
 #include "napi/native_api.h"
 
 // 各模块的导出注册
@@ -85,4 +29,3 @@ extern "C" __attribute__((constructor)) void RegisterEntryModule(void)
 {
     napi_module_register(&demoModule);
 }
->>>>>>> cpp
